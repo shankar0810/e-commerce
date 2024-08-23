2fc38f36dc5b9dc54603307e9570cbd4a06bd2f6
shankar0810 <shankarmerugu14@gmail.com>
2024-08-23 12:49:22 +0530

    first commit

---
+++ src/main/java/com/gfg/springsonarqubedemo/SpringSonarQubeDemoApplication.java
@@ -0,0 +1,16 @@
+package com.gfg.springsonarqubedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Main class to start the Spring SonarQube Demo application.
+ */
+@SpringBootApplication
+public class SpringSonarQubeDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSonarQubeDemoApplication.class, args);
+	}
+
+}
